b2152b599868bca63668b9c0cb572b14dee55b48
immprada <im.mprada@gmail.com>
2023-05-02 15:30:52 -0500

    add scopes to Event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,9 @@ class Event < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
 
+  scope :past, -> { where('start_date <= ?', DateTime.now) }
+  scope :future, -> { where('start_date > ?', DateTime.now) }
+
   belongs_to :creator, class_name: 'User'
   has_many :event_attendees, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendees
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe Event, type: :model do
     it { is_expected.to have_many(:attendees).through(:event_attendees) }
     it { is_expected.to have_many(:event_attendees).dependent(:destroy) }
   end
+
+  describe 'scopes' do
+    let(:past_event) { create(:event, start_date: 1.day.ago, end_date: 1.day.ago + 1.hour) }
+    let(:future_event) { create(:event, start_date: 1.day.from_now, end_date: 1.day.from_now + 1.hour) }
+
+    describe '.past' do
+      it 'returns past events' do
+        expect(described_class.past).to eq([past_event])
+      end
+    end
+
+    describe '.future' do
+      it 'returns future events' do
+        expect(described_class.future).to eq([future_event])
+      end
+    end
+  end
 end
